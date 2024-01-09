9ef400ae4eba3f74f2111967de4be813ea0b42e8
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-01-09 16:31:56 +0530

    login f

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+const loginForm=document.querySelector('#loginForm');
+const messageElement=document.querySelector('#message')
+
+
+
+loginForm.addEventListener('submit',onSubmit)
+
+async function onSubmit(e){
+    e.preventDefault();
+
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    messageElement.innerHTML=''
+
+
+    let data={
+        email,
+        password
+    }
+    console.log(data);
+    e.target.email.value='';
+    e.target.password.value=''
+
+    try {
+        const res = await axios.post('login', data);
+        if (res.status === 200) {
+            messageElement.innerText = res.data.message;
+            messageElement.className = 'message-success';
+            localStorage.setItem('token',res.data.token)
+            window.location.href = 'add-expense';
+        }
+    } catch (error) {
+        if (error.response) {
+            messageElement.innerText = error.response.data.message;
+            messageElement.className = 'message-error';
+        } else if (error.request) {
+            messageElement.innerText = 'No response from server';
+            messageElement.className = 'message-error';
+        } else {
+            messageElement.innerText = 'Error in setting up the request';
+            messageElement.className = 'message-error';
+        }
+    }
+}
