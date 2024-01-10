67f2f33dec8edf2179b50a428c9dbea650db0c77
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-01-10 12:11:29 +0530

    chat model

--- app.js
+++ app.js
@@ -9,13 +9,27 @@ const sequelize=require('./util/database');
 const app=express()
 const cors=require("cors")
 
+const User=require('./models/user');
+const Chat=require('./models/chatdata')
+
+const userRoutes=require('./routes/user')
+
+
+
+
 app.use(cors())
+// app.use(express.static(__dirname));
 app.use(bodyParser.json({ extended: false }));
 app.use(express.static('public'));
+app.use(userRoutes)
 
+
+User.hasMany(Chat)
+Chat.belongsTo(User)
 
 sequelize
 .sync()
+// .sync({force:true})
 .then(result=>{
     app.listen(4000);
 })
