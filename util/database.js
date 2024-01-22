439eb8e36cd174c34307761a4dd7289a0e0b4260
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-01-22 17:23:15 +0530

    again

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize("WEChat", "root","nodecomplete", {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: "localhost",
-  // logging:false
+  host: process.env.DB_HOST,
+  logging:false
 });
 
 module.exports = sequelize;
