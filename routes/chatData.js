aa598b438f74db87b9bd890122e9937e4ea7fb30
gauravyadav199623 <gauravyadav199623@gmail.com>
2024-01-22 17:17:09 +0530

    multer

--- routes/chatData.js
+++ routes/chatData.js
@@ -5,7 +5,7 @@ const express=require('express')
 const chatController=require('../controller/chat');
 const UserAuth=require('../MiddleWare/auth')
 
-const multerMiddleware = require('../middleware/multer')
+const multerMiddleware = require('../MiddleWare/multer')
 const upload = multerMiddleware.multer.array('media');
 
 const router=express.Router()
